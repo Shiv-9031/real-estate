946627086037521bc38f13a9badff8685b87ed94
shiv kumar <kumarshiv9031127789@gmail.com>
2024-07-18 11:00:46 +0530

    add index file in pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-import Page1 from "./pages/page1/Page1";
-import Page2 from "./pages/page2/Page2";
-import Page3 from "./pages/page3/Page3";
-import Page4 from "./pages/page4/Page4";
-import Page5 from "./pages/page5/Page5";
-import Page6 from "./pages/Page6/Page6";
-import Page7 from "./pages/page7/Page7";
-import Page8 from "./pages/page8/Page8";
-import Page9 from "./pages/page9/Page9";
+import {
+  Page1,
+  Page2,
+  Page3,
+  Page4,
+  Page5,
+  Page6,
+  Page7,
+  Page8,
+  Page9,
+} from "./pages";
+
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoutes from "./helper/ProtectedRoutes";
 import { ToastContainer } from "react-toastify";
@@ -89,15 +92,14 @@ function App() {
   ];
 
   return (
-    <>
+    <div>
       <Routes>
         {pages.map((page, index) => {
           return <Route path={page.path} element={page.element} />;
         })}
       </Routes>
-
       <ToastContainer />
-    </>
+    </div>
   );
 }
 
